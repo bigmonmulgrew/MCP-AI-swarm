733c003d21063768e78a168ef51c5dadeea3b350
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2025-11-10 20:55:20 +0000

    split data drone class and config

--- MCP-Data/main.py
+++ MCP-Data/main.py
@@ -1,18 +1,4 @@
-# data/main.py
-from fastapi import FastAPI
-from common import DroneQueryObject, BaseDroneServer
-
-class DataDrone(BaseDroneServer):
-    def _register_query_endpoint(self):
-        @self.app.post("/query")
-        async def handle_query(dqo: DroneQueryObject):
-            print(f"Received query: {dqo.Query}")
-            return {
-                "msg": "Returned some data successfully.",
-                "images": [],
-                "files": [],
-                "videos": [],
-            }
+from drone import DataDrone
 
 drone = DataDrone(
     name="MCP Data server",

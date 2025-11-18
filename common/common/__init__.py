8a9f35bdfa001545f95ad5b40492e8ab91bba3aa
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2025-11-18 13:16:25 +0000

    Added AI query object to stucts
    Used in queing

--- common/common/__init__.py
+++ common/common/__init__.py
@@ -1,3 +1,3 @@
-from .structs import DroneOnlineObject, DroneQueryObject, SenderHistory, Message
+from .structs import DroneOnlineObject, DroneQueryObject, SenderHistory, Message, UserQuery, AIQuery
 from .bootstrap import BOOT_IMAGE, BOOT_MCPS_ONLINE_RESPONSE
 from .base_drone import BaseDroneServer

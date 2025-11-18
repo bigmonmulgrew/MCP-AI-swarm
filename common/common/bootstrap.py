b1358fb28ec53f45c0edcd7461a51b7e4df9c8ba
Lee-Roy <ltd1999@hotmail.co.uk>
2025-11-18 13:32:05 +0000

    Fixed bootstrap bugs

--- common/common/bootstrap.py
+++ common/common/bootstrap.py
@@ -1,7 +1,7 @@
-from bloc_image import bloc_image
-from camera_data import camera_data
-from local_data import local_data
-BOOT_IMAGE=bloc_image
+from .bloc_image import bloc_image1
+from .camera_data import camera_data
+from .local_data import local_data
+BOOT_IMAGE=bloc_image1
 CAM_DATA=camera_data
 LOCAL_DATA=local_data
 DOMAIN_DATA="Law (Rule): All storage sites (of dangerous materials) must be constantly monitored"

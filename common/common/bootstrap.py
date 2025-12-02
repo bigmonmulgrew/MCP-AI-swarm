f036b2205e29799b045319fcf8723ef945366def
Lee-Roy <ltd1999@hotmail.co.uk>
2025-12-02 14:30:14 +0000

    Added bootstrap data

--- common/common/bootstrap.py
+++ common/common/bootstrap.py
@@ -2,8 +2,10 @@ from common.bootstrap_extra.mcps_online_response import mcps_online_response
 from .bloc_image import bloc_image1
 from .camera_data import camera_data
 from .local_data import local_data
+from .t_light_rules import rules
 BOOT_IMAGE=bloc_image1
 CAM_DATA=camera_data
 LOCAL_DATA=local_data
 DOMAIN_DATA="Law (Rule): All storage sites (of dangerous materials) must be constantly monitored"
 BOOT_MCPS_ONLINE_RESPONSE=mcps_online_response
+T_LIGHT_RULES=rules

3a3ccfb1451a7c7537db21d395ce6335c643106f
MutantGinga <haydenwilliamhefford@yahoo.com>
2025-12-16 13:46:34 +0000

    Added epoch time conversion
    Converts dates stored in LOCAL_DATA to epoch time (runs in data drone)

--- MCP-Data/drone.py
+++ MCP-Data/drone.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from common import DroneQueryObject, BaseDroneServer, Message, BOOT_IMAGE, CAM_DATA, LOCAL_DATA
 
 class DataDrone(BaseDroneServer):
@@ -5,12 +6,27 @@ class DataDrone(BaseDroneServer):
         @self.app.post("/query")
         async def handle_query(dqo: DroneQueryObject):
             print(f"Received query: {dqo.Query}")
+
+            epoch_times = []
+
+            for item in LOCAL_DATA.values():
+                date_time = item.split("-")
+                dt = datetime(int(date_time[0]), int(date_time[1]), int(date_time[2]), tzinfo=timezone.utc)
+                epoch_time = dt.timestamp()
+                epoch_times.append(epoch_time)
+            
+
+            #epoch_time = datetime()
+
+            
+            # epoch_time = dt.timestamp()
+            # print(epoch_time)
             
             payload = Message(
                 role = "bot"   ,                            # Message sender type
                 Msg = "Structured \{\{I1}} Data",           # The actual message  # {{I1}} token for image replacement example only, no defined format yet
                 Images= ["dsadas"],
-                structuredMsg =  [CAM_DATA, LOCAL_DATA],  # Structured data strings
+                structuredMsg =  [CAM_DATA, epoch_times],  # Structured data strings
                 Files = [],
                 Videos = []
             )

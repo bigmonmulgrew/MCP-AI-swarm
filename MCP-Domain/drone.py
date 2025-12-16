196311e2d1007478fda2494d9487c0df4a3f8074
MutantGinga <haydenwilliamhefford@yahoo.com>
2025-12-16 14:29:51 +0000

    test ai call in domain drone

--- MCP-Domain/drone.py
+++ MCP-Domain/drone.py
@@ -1,4 +1,7 @@
+import requests
 from common import DroneQueryObject, BaseDroneServer, Message
+
+API_URL = "http://localhost:9090/query"
 
 class DomainDrone(BaseDroneServer):
     def _register_subclass_endpoints(self):
@@ -14,9 +17,19 @@ class DomainDrone(BaseDroneServer):
                 Message: Response message with processed data
             """
             print(f"Received query: {dqo.Query}")
-            
-            # TODO: Implement your query handling logic here
-            # Example: Process the query and return structured data
+
+            ai_payload = {
+                "prompt": "tell me about lightning mcqueen",
+                "model": "qwen3:1.7b",
+                "options": {}
+            }
+
+            try:
+                response = requests.post(API_URL, json=ai_payload, timeout = 120)
+                response.raise_for_status()
+                print(response.json())
+            except Exception as e:
+                print(e)
             
             payload = Message(
                 role="bot",

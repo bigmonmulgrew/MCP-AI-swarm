b6747ca8fea0dc42331478a5b7728ee10bdd3b2d
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-01-13 17:33:47 +0000

    fixed a typo in common structs import

--- common/common/__init__.py
+++ common/common/__init__.py
@@ -1,3 +1,3 @@
-from .structs import DroneOnlineObject, DroneQueryObject, SenderHistory, Message, UserQuery, AiQuery
+from .structs import DroneOnlineObject, DroneQueryObject, SenderHistory, Message, UserQuery, AIQuery
 from .bootstrap import BOOT_IMAGE, camera_data, local_data, BOOT_MCPS_ONLINE_RESPONSE
 from .base_drone import BaseDroneServer

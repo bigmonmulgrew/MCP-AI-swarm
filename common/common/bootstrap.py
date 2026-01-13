057dc119a42e64447f4ff4d7c99a863f5082f62f
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-01-13 20:15:35 +0000

    added placeholder for system query

--- common/common/bootstrap.py
+++ common/common/bootstrap.py
@@ -1,5 +1,6 @@
 from common.bootstrap_extra.mcps_online_response import mcps_online_response
 from common.bootstrap_extra.blochub_query_response import bloc_response
+from common.bootstrap_extra.system_query import system_query
 from common.bootstrap_extra.orc_query_01 import query_response as orc_response_01
 from .bloc_image import bloc_image1
 from .camera_data import camera_data
@@ -12,3 +13,4 @@ DOMAIN_DATA="Law (Rule): All storage sites (of dangerous materials) must be cons
 BOOT_MCPS_ONLINE_RESPONSE=mcps_online_response
 BOOT_BLOC_HUB_RESPONSE=bloc_response
 BOOT_QUERY_RESPOSNE_01=orc_response_01
+BOOT_SYSTEM_QUERY=system_query
---
+++ common/common/bootstrap_extra/system_query.py
@@ -0,0 +1,5 @@
+system_query = """
+What are the risks associated with the attached data. Use the provided domain and local data to determine a risk framework and provide a risk score response as well as a short risk summery. 50 word max.
+
+Provide your response in a JSON formatted 
+"""

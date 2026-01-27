20056d7a8156774f6f6f371a6d9b0a57f3dc23b1
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-01-27 13:14:23 +0000

    added sample env frorm emiz (reduced)

--- MCP-Domain-XL/main.py
+++ MCP-Domain-XL/main.py
@@ -1,10 +1,10 @@
 from drone import DomainDrone
 
 drone = DomainDrone(
-    name="MCP Domain server",
+    name="MCP Domain XL server",
     category="Unstructured",
-    env_host_key="MCP_DOMAIN_HOST",
-    env_port_key="MCP_DOMAIN_PORT_E",
+    env_host_key="MCP_DOMAIN_XL_HOST",
+    env_port_key="MCP_DOMAIN_XL_PORT_E",
 )
 
 app = drone.app
